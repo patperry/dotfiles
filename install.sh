fe10c44bf413b96e64c6a38b3e59cafd5419bdeb
Patrick Perry <patperry@gmail.com>
2010-04-28 12:15:54 -0400

    More portable files (rip off http://stefaanlippens.net/my_bashrc_aliases_profile_and_other_stuff)

--- install.sh
+++ install.sh
@@ -2,11 +2,24 @@
 
 pushd $HOME
 rm -rf .bash         && ln -s .dotfiles/bash      .bash
+rm -f  .bash_aliases && ln -s .bash/bash_aliases  .bash_aliases
 rm -f  .bash_profile && ln -s .bash/bash_profile  .bash_profile
-rm -rf .cabal        && ln -s .dotfiles/cabal     .cabal
+rm -f  .bashrc       && ln -s .bash/bashrc        .bashrc
+
 rm -f  .gitconfig    && ln -s .dotfiles/gitconfig .gitconfig
 rm -f  .gitignore    && ln -s .dotfiles/gitignore .gitignore
 rm -rf .vim          && ln -s .dotfiles/vim       .vim
 rm -f  .vimrc        && ln -s .vim/vimrc          .vimrc
+
+rm -rf .cabal
+rm -f  .profile
+if [[ `expr "$OSTYPE" : 'darwin'` ]]; then
+	ln -s .dotfiles/cabal.darwin     .cabal
+	ln -s .dotfiles/profile.darwin   .profile
+else
+	ln -s .dotfiles/cabal.default     .cabal
+	ln -s .dotfiles/profile.default   .profile
+fi
+
 popd
 
